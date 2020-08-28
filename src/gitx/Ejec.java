9f6f29d789f628c4b019d66eaed2b09a4a9f0566
bdvlupr <bogadovd@hotmail.com>
2020-08-28 19:36:16 -0300

    version 4

--- src/gitx/Ejec.java
+++ src/gitx/Ejec.java
@@ -1,10 +1,49 @@
 package gitx;
+import java.io.*;
 
 public class Ejec {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int contador=0;
+		int biteado[]=new int[2810752];
+		
+		
+		try {
+			FileInputStream jpg=new FileInputStream("C:/Users/Familia rocker/Desktop/proyect_git/otras_i/fondo hd3.jpg");
+		
+			boolean xx= false;
+			
+			while (!xx){
+				int x3=jpg.read();
+				
+				if(x3!=-1)
+					
+					biteado[contador]=x3;
+				else 
+						xx=true;
+				
+			
+				contador++;
+			}
+			jpg.close();
+			
+			
+		} catch (IOException e) {
+			//
+			
+			e.printStackTrace();
+		}
+		System.out.println(contador);
 	}
-
 }
+
+
+
+		
+		
+		 
+	
+
+
